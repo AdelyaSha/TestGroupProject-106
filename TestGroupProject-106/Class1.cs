89bd894bd8e275cf037f2f47820cfb7199514572
Аделя Шагабиева <AAShagabieva@stud.kpfu.ru>
2023-03-02 13:27:29 +0300

    override ToString();

---
+++ TestGroupProject-106/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGroupProject_106
+{
+    class Class1
+    {
+        public override string ToString()
+        {
+            return "Hello there!";
+        }
+    }
+}
